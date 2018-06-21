3f9facaafe48bf1513e3bc3b4ceb33d6ebc9b843
bhartigupta <bhartigupta63@gmail.comgit config --global user.name bhartiguptagit config --global user.email bhartigupta63@gmail.com>
2018-06-21 21:02:49 +0530

    assignment 2

---
+++ assignment2.py
@@ -0,0 +1,34 @@
+ #1
+
+ print("i am bharti gupta")
+
+ #2
+
+ print("Bharti"+"Gupta")
+
+ #3
+
+ x=input("enter first number")
+ y = input("enter second number")
+ z = input("enter third number")
+ print("first number= ",x)
+ print("second number= ",y)
+ print("third number= ",z)
+
+ #4
+
+ print("Let's get started")
+
+ #5
+
+ s = "Acadview"
+ course = "Python"
+ fees = 5000
+ print("joined %s in %s for fees %d" %(s,course,fees))
+
+ #6
+
+ name = "Tony stark"
+ salary = 1000000
+ print('%s''%d' %(name,salary))
+ 
